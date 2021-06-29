9fcbc2b91f8b1b5c9763a37b197b436dd81624be
georgelyjo <georgelyjo@gmail.com>
2021-06-29 12:26:37 +0530

    still in progress

---
+++ subdomain-enum.sh
@@ -0,0 +1,31 @@
+if [ -z "$1" ]
+then
+	echo "please input the name of the domain"
+else
+	dir=~/bug/$1
+	mkdir -p $dir
+	echo "subfinder"
+	subfinder -d $1 | anew  $dir/$1_subdomains; 
+	echo "assetfinder"
+	assetfinder  --subs-only $1  |  anew  $dir/$1_subdomains; 
+	echo "crt.sh"
+	curl -s https://crt.sh/\?q\=\%.$1\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | anew $dir/$1_subdomains;
+	echo "rapiddns"
+	curl -s "https://rapiddns.io/subdomain/$1?full=1#result" | grep "<td><a" | cut -d '"' -f 2 | grep http | cut -d '/' -f3 | sed 's/#results//g' | sort -u | anew $dir/$1_subdomains;
+	echo "bufferoverflow"
+	curl -s https://dns.bufferover.run/dns?q=.$1 |jq -r .FDNS_A[]|cut -d',' -f2|sort -u | anew $dir/$1_subdomains;
+	echo "riddle"
+	curl -s "https://riddler.io/search/exportcsv?q=pld:$1" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | anew $dir/$1_subdomains;
+	echo "certspoter"
+	curl -s "https://certspotter.com/api/v1/issuances?domain=$1&include_subdomains=true&expand=dns_names" | jq .[].dns_names | tr -d '[]"\n ' | tr ',' '\n' | anew $dir/$1_subdomains;
+	echo "web-archive"
+	curl -s "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" | sort -u | anew $dir/$1_subdomains;
+	echo "jdlc"
+	curl -s "https://jldc.me/anubis/subdomains/$1" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | anew $dir/$1_subdomains;
+	echo "sonar"
+	curl --silent https://sonar.omnisint.io/subdomains/$1  | grep -oE "[a-zA-Z0-9._-]+\.twitter.com" | sort -u | anew $dir/$1_subdomains;
+	echo "finddomain"
+	findomain -t $1 | anew $dir/$1_subdomains;
+	echo "knockpy"
+	knockpy $1 | anew $dir/$1_subdomains;
+fi
