89d037724cdf82e3f79c7c935e34b89c3fc9eb59
Georgely Jo Santino <47821682+georgelyjo@users.noreply.github.com>
2021-10-04 12:46:25 +0530

    Create subdomain-enum-tools.sh

---
+++ subdomain-enum-tools.sh
@@ -0,0 +1,14 @@
+if [ -z "$1" ]
+then
+	echo "please input the name of the domain"
+else
+    dir=~/subdomains/$1
+  	mkdir -p $dir
+    echo "finddomain"
+    findomain -t $1 | anew $dir/$1_subdomains;
+    echo "knockpy"
+    knockpy $1 | anew $dir/$1_subdomains;
+    echo "subfinder"
+    subfinder -d $1 | anew  $dir/$1_subdomains; 
+    echo "assetfinder"
+    assetfinder  --subs-only $1  |  anew  $dir/$1_subdomains; 
