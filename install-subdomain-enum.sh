3fc46b1899716086e1b6a5d03adb98a80a1bf38d
Georgely Jo Santino <47821682+georgelyjo@users.noreply.github.com>
2021-11-26 21:45:06 +0530

    Update install-subdomain-enum.sh
    new tools added

--- install-subdomain-enum.sh
+++ install-subdomain-enum.sh
@@ -4,3 +4,9 @@ curl https://sh.rustup.rs -sSf | sh
 git clone https://github.com/findomain/findomain.git && cd findomain && cargo build --release && sudo cp target/release/findomain /usr/bin/findomain
 #knockpy
 git clone https://github.com/guelfoweb/knock.git && python3 setup.py install
+#assetfinder
+go get -u github.com/tomnomnom/assetfinder
+#subfinder
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+#subdomanizer
+sudo git clone https://github.com/nsonaniya2010/SubDomainizer.git && cd  SubDomainizer && pip3 install -r requirements.txt && sudo mv SubDomainizer.py SubDomainizer && mv SubDomainizer /usr/bin/subdomanizer
