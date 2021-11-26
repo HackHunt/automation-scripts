d1b7f4032f4b63a5c29eb4514dd8da82863013b1
Georgely Jo Santino <47821682+georgelyjo@users.noreply.github.com>
2021-11-26 21:58:01 +0530

    Update install-subdomain-enum.sh

--- install-subdomain-enum.sh
+++ install-subdomain-enum.sh
@@ -10,3 +10,5 @@ go get -u github.com/tomnomnom/assetfinder
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 #subdomanizer
 sudo git clone https://github.com/nsonaniya2010/SubDomainizer.git && cd  SubDomainizer && pip3 install -r requirements.txt && sudo mv SubDomainizer.py SubDomainizer && mv SubDomainizer /usr/bin/subdomanizer
+#Amass
+sudo snap install amass
